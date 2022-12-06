e82123756b4b003963cd967a357091d988a425d8
Santiago Bruno <santibrunovm@gmail.com>
2022-12-06 16:39:40 -0300

    conflicto home

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-import React from "react";
-import { useContextGlobal } from "../Components/utils/ContextGlobal";
-import Card from "../Components/Card";
-import { Link } from "react-router-dom";
-=======
 import React from 'react'
 import { useContextGlobal } from '../Components/utils/ContextGlobal'
 import Card from '../Components/Card'
->>>>>>> a9c4eb86b8cea459e6dccff58135c5e2ec09886a
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -17,17 +10,8 @@ const Home = () => {
   return (
     <main className="">
       <h1>Home</h1>
-<<<<<<< HEAD
-      <div className="card-grid">
-        {dentist.map((dentist) => (
-          <Link to={`/detail/${dentist.id}`} key={dentist.id}>
-            <Card key={dentist.id} />
-          </Link>
-        ))}
-=======
       <div className='card-grid'>
         {dentist.map(dentist => (<Card key={dentist.id} data={dentist} />))}
->>>>>>> a9c4eb86b8cea459e6dccff58135c5e2ec09886a
       </div>
     </main>
   );
